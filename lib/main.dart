ffcdafd6d5061f4ab1a4d1d746b6e160f97d0821
AjibsBaba <samuelajibade22@gmail.com>
2022-12-24 19:58:01 +0100

    added firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Acefood',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
             primarySwatch: Colors.red,
             primaryColor: Colors.red,
